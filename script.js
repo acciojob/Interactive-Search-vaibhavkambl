89f4351cf1778898a06d2c38ad7db9882f9ebcc8
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-20 13:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function activateSearch() {
+      var searchContainer = document.getElementById('searchContainer');
+      var searchInput = document.getElementById('searchInput');
+
+      searchContainer.classList.add('active');
+      searchInput.focus();
+    }
